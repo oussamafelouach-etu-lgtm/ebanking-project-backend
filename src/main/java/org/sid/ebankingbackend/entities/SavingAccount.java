1eec96bf251b35d984c5f92e59175612cf67b6e3
oussama <oussama.felouach-etu@etu.univh2c.ma>
2026-05-05 22:06:09 +0200

    first commit

---
+++ src/main/java/org/sid/ebankingbackend/entities/SavingAccount.java
@@ -0,0 +1,14 @@
+package org.sid.ebankingbackend.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@DiscriminatorValue("SA")
+public class SavingAccount extends  BankAccount{
+    private double interestRate;
+}
